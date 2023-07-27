fffce00f22f24e5046a06aada072fceedfe41350
RAYYAN057 <139691945+RAYYAN057@users.noreply.github.com>
2023-07-27 08:57:47 +0500

    Add files via upload
    This is a program in python using for loop to find average of different students in 3 different courses.

---
+++ FOR LOOP.py
@@ -0,0 +1,29 @@
+a = input('Please enter the total number of students:')
+print(type(int(a)))
+overall_list = []
+sum_1=0
+for my_counter in list(range(int(a))):
+    student_marks_list = []
+    sum = 0
+
+
+    for my_counter_2 in list(range(int(3))):
+
+        temp = input('Please enter the marks of students:')
+        sum = sum + (int (temp))
+
+
+
+
+
+    
+    average = sum/ 3
+    sum_1 = sum_1+average
+
+
+print(sum)
+
+
+average = sum_1/(int (a))
+
+print(average)
